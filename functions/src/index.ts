682b6df1fba1853dcefd65e5f50ddb5ca2a8b50d
Tuval Daskal <tuval.daskal@gmail.com>
2026-01-29 10:50:58 +0200

    fix(functions): allow firebase-instance-id-token header in CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -191,7 +191,7 @@ export const scrapeProductDetails = functions
         res.set('Access-Control-Allow-Origin', '*');
         if (req.method === 'OPTIONS') {
             res.set('Access-Control-Allow-Methods', 'POST');
-            res.set('Access-Control-Allow-Headers', 'Content-Type');
+            res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, firebase-instance-id-token');
             res.status(204).send('');
             return;
         }
